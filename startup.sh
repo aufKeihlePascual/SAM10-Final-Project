f12c39115efde87375de193b2fcc95b48d4346a9
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-11-12 02:00:57 +0800

    updated startup.sh

--- startup.sh
+++ startup.sh
@@ -4,5 +4,14 @@
 # Set web root to Laravel public folder
 export DOCUMENT_ROOT=/home/site/wwwroot/public
 
-# Start PHP-FPM
-php-fpm
+# Run migrations
+php /home/site/wwwroot/artisan migrate --force
+
+# Clear & cache Laravel caches
+php /home/site/wwwroot/artisan cache:clear
+php /home/site/wwwroot/artisan route:cache
+php /home/site/wwwroot/artisan config:cache
+php /home/site/wwwroot/artisan view:cache
+
+# Storage link (optional if not using S3/Blob)
+php /home/site/wwwroot/artisan storage:link
